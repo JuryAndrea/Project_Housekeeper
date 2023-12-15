739341e9681a79377ca6c6260cd78f96bd2e57e2
JuryAndrea <jury.donofrio@gmail.com>
2023-12-15 13:44:10 +0100

    UI improvements and login with NFC TODO: add SharedPreferences to save the login status

--- app/src/main/java/com/example/stepappv6/LoginActivity.java
+++ app/src/main/java/com/example/stepappv6/LoginActivity.java
@@ -1,10 +1,21 @@
 package com.example.stepappv6;
 
 
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,17 +23,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button skipButton;
+
+    private TextView text;
 
     private EditText editTextEmail;
     private EditText editTextPassword;
@@ -36,12 +55,18 @@ public class LoginActivity extends AppCompatActivity {
 
     private SharedPreferences.Editor editor;
 
+    private NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    private IntentFilter[] intentFiltersArray;
+    private String[][] techListsArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         skipButton = findViewById(R.id.skip);
+        text = findViewById(R.id.text_msg);
 
         skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,75 +77,122 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        //TODO: NFC reader and SharedPreferences
-
-        /*
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.header_logo, null);
-        getSupportActionBar().setCustomView(view);
-
-
-        buttonLogin = findViewById(R.id.login);
-        editTextPassword = findViewById(R.id.editTextTextPassword);
-        editTextEmail = findViewById(R.id.editTextTextEmailAddress);
-        checkBoxLogin = findViewById(R.id.checkBoxLogin);
-
-
-        preference = PreferenceManager.getDefaultSharedPreferences(this);
-        editor = preference.edit();
-
-        if (preference.contains("checked") && preference.getBoolean("checked", false) == true) {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "NFC is not supported on this device.", Toast.LENGTH_SHORT).show();
             finish();
-        } else {
-            checkBoxLogin.setChecked(false);
+            return;
         }
 
-        jsonObject = JsonFileReader.readJsonFromAssets(this, "credentials.json");
-        if (jsonObject != null) {
-            try {
-                // Access JSON data here
-                email = jsonObject.getString("mail");
-                password = jsonObject.getString("key");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+        if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC is not enabled. Please enable NFC in device settings.", Toast.LENGTH_SHORT).show();
         }
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editTextEmail.getText().toString().equals(email) && editTextPassword.getText().toString().equals(password)) {
-                    //https://stackoverflow.com/questions/38975076/how-to-apply-shared-preference-to-checkbox
-                    if (checkBoxLogin.isChecked()) {
-
-                        editor.putBoolean("checked", true);
-                        editor.apply();
-
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-
-                    } else {
-                        editor.putBoolean("checked", false);
-                        editor.apply();
-
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                } else {
-                    Toast.makeText(getApplicationContext(), "User not found", Toast.LENGTH_LONG).show();
-                }
+        handleIntent(getIntent());
+    }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableNfcForegroundDispatch();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disableNfcForegroundDispatch();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        Log.d("JURY", "Intent Action: " + action);
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String tagId = byteArrayToHexString(tag.getId());
+            Log.d("JURY", "Tag: " + tagId);
+
+            if (tagId.equals("1480275E")){
+                Log.d("JURY", "pluto");
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+                //TODO: add SharedPreferences to save the login status
             }
-        });
-        */
 
+//            if (tag != null) {
+//                Log.d("JURY", "Tag ID: " + Arrays.toString(tag.getId()));
+//                NdefMessage ndefMessage = readNdefMessage(tag);
+//                Log.d("JURY", "NDEF Message: " + ndefMessage);
+//                if (ndefMessage != null) {
+//                    String tagData = new String(ndefMessage.getRecords()[0].getPayload());
+//                    text.setText("JURY " + tagData);
+//                } else {
+//                    Log.e("JURY", "Failed to read NDEF message from tag.");
+//                }
+//            }
+        }
+    }
+
+    private String byteArrayToHexString(byte[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : array) {
+            sb.append(String.format("%02X", b));
+        }
+        return sb.toString();
+    }
+
+//    private NdefMessage readNdefMessage(Tag tag) {
+//        Ndef ndef = Ndef.get(tag);
+//        if (ndef != null) {
+//            try {
+//                ndef.connect();
+//                return ndef.getNdefMessage();
+//            } catch (IOException e) {
+//                Log.e("JURY", "Error reading NDEF message", e);
+//            } catch (FormatException e) {
+//                Log.e("JURY", "Error formatting NDEF message", e);
+//                throw new RuntimeException(e);
+//            } finally {
+//                try {
+//                    ndef.close();
+//                } catch (IOException e) {
+//                    Log.e("JURY", "Error closing NDEF connection", e);
+//                }
+//            }
+//        }
+//        return null;
+//    }
+
+    private void enableNfcForegroundDispatch() {
+        Intent intent = new Intent(this, getClass());
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+
+        IntentFilter[] intentFilters = new IntentFilter[]{
+                new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED),
+                new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED),
+                new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED)
+        };
+
+        String[][] techLists = new String[][]{
+                new String[]{Ndef.class.getName()},
+                new String[]{NdefFormatable.class.getName()}
+        };
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, techLists);
+    }
+
+    private void disableNfcForegroundDispatch() {
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
     }
 
 }
--- app/src/main/java/com/example/stepappv6/ui/Room/RoomFragment.java
+++ app/src/main/java/com/example/stepappv6/ui/Room/RoomFragment.java
@@ -13,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -29,6 +30,12 @@ public class RoomFragment extends Fragment {
     FragmentRoomBinding binding;
 
     private Button status;
+    private ImageView status_image1;
+    private ImageView status_image2;
+    private ImageView status_image3;
+    private ImageView status_image4;
+    private ImageView status_image5;
+    private ImageView status_image6;
 //    private Button logout;
 
 
@@ -41,6 +48,14 @@ public class RoomFragment extends Fragment {
         binding = FragmentRoomBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        status_image1 = root.findViewById(R.id.status_room1);
+        status_image2 = root.findViewById(R.id.status_room2);
+        status_image3 = root.findViewById(R.id.status_room3);
+        status_image4 = root.findViewById(R.id.status_room4);
+        status_image5 = root.findViewById(R.id.status_room5);
+        status_image6 = root.findViewById(R.id.status_room6);
+
+        // dirty = 0, cleaning = 1, cleaned = 2, ready = 3
         String[] status = new String[]{"Dirty", "Cleaning", "Cleaned", "Ready"};
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getContext(), R.layout.dropdown_item, status);
 
@@ -59,41 +74,87 @@ public class RoomFragment extends Fragment {
         AutoCompleteTextView autoCompleteTextView5 = root.findViewById(R.id.filled5);
         autoCompleteTextView5.setAdapter(arrayAdapter);
 
+        AutoCompleteTextView autoCompleteTextView6 = root.findViewById(R.id.filled6);
+        autoCompleteTextView5.setAdapter(arrayAdapter);
+
         autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getContext(), autoCompleteTextView.getText().toString(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getContext(), autoCompleteTextView.getText().toString() + " pos " + i, Toast.LENGTH_SHORT).show();
+                int pos = choose_status(i);
+                set_image(pos, status_image1);
             }
         });
 
         autoCompleteTextView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getContext(), autoCompleteTextView2.getText().toString(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getContext(), autoCompleteTextView2.getText().toString(), Toast.LENGTH_SHORT).show();
+                int pos = choose_status(i);
+                set_image(pos, status_image2);
             }
         });
 
         autoCompleteTextView3.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getContext(), autoCompleteTextView3.getText().toString(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getContext(), autoCompleteTextView3.getText().toString(), Toast.LENGTH_SHORT).show();
+                int pos = choose_status(i);
+                set_image(pos, status_image3);
             }
         });
 
         autoCompleteTextView4.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getContext(), autoCompleteTextView4.getText().toString(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getContext(), autoCompleteTextView4.getText().toString(), Toast.LENGTH_SHORT).show();
+                int pos = choose_status(i);
+                set_image(pos, status_image4);
             }
         });
 
         autoCompleteTextView5.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getContext(), autoCompleteTextView5.getText().toString(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getContext(), autoCompleteTextView5.getText().toString(), Toast.LENGTH_SHORT).show();
+                int pos = choose_status(i);
+                set_image(pos, status_image5);
+            }
+        });
+
+        autoCompleteTextView6.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Toast.makeText(getContext(), autoCompleteTextView5.getText().toString(), Toast.LENGTH_SHORT).show();
+                int pos = choose_status(i);
+                set_image(pos, status_image6);
             }
         });
 
         return root;
     }
+
+    private int choose_status(int pos){
+        if (pos == 0)
+            return 0;
+        else if (pos == 1)
+            return 1;
+        else if (pos == 2)
+            return 2;
+        else
+            return 3;
+    }
+
+    private void set_image(int pos, ImageView status_image){
+        if (pos == 0)
+            status_image.setImageResource(R.drawable.dirty_50);
+        else if (pos == 1)
+            status_image.setImageResource(R.drawable.cleaning_64);
+        else if (pos == 2)
+            status_image.setImageResource(R.drawable.cleaned_64);
+        else
+            status_image.setImageResource(R.drawable.ready_50);
+    }
+
+
 }
